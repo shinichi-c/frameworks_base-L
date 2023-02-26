a0fca5b7fba2d1df7edae115283022d18c360395
Pranav Vashi <neobuddy89@gmail.com>
2023-02-26 16:57:25 +0530

    AppLock: Allow locking all apps visible in launcher
    Change-Id: I041b2b766defb84c593862df235e4526f9d37130
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/lunaris/Utils.java
+++ core/java/com/android/internal/util/lunaris/Utils.java
@@ -28,6 +28,7 @@ import android.content.om.OverlayInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.AsyncTask;
 import android.os.PowerManager;
 import android.os.SystemClock;
@@ -37,6 +38,7 @@ import android.util.Log;
 
 import com.android.internal.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
@@ -101,6 +103,24 @@ public class Utils {
         } catch (PackageManager.NameNotFoundException notFound) {
             return false;
         }
+    }
+
+    public static List<String> launchablePackages(Context context) {
+        List<String> list = new ArrayList<>();
+
+        Intent filter = new Intent(Intent.ACTION_MAIN, null);
+        filter.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> apps = context.getPackageManager().queryIntentActivities(filter,
+                PackageManager.GET_META_DATA);
+
+        int numPackages = apps.size();
+        for (int i = 0; i < numPackages; i++) {
+            ResolveInfo app = apps.get(i);
+            list.add(app.activityInfo.packageName);
+        }
+
+        return list;
     }
 
     public static void switchScreenOff(Context ctx) {
