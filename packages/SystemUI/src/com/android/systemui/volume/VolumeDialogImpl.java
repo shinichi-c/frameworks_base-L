b32147ff4ab2f5f78e816e4e875c673e02fe507b
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-17 08:46:47 +0800

    SystemUI: Introduce VolumeUtils
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -184,6 +184,9 @@ import com.android.internal.util.android.VibrationUtils;
 public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         ConfigurationController.ConfigurationListener,
         ViewTreeObserver.OnComputeInternalInsetsListener {
+
+    private final VolumeUtils mVolumeUtils;
+
     private static final String TAG = Util.logTag(VolumeDialogImpl.class);
 
     private static final long USER_ATTEMPT_GRACE_PERIOD = 1000;
@@ -408,6 +411,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             VolumeDialogInteractor interactor) {
         mContext =
                 new ContextThemeWrapper(context, R.style.volume_dialog_theme);
+        mVolumeUtils = new VolumeUtils(mContext);
         mHandler = new H(looper);
         mVibratorHelper = vibratorHelper;
         mMSDLPlayer = msdlPlayer;
@@ -564,6 +568,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         if (mDevicePostureController != null) {
             mDevicePostureController.removeCallback(mDevicePostureControllerCallback);
         }
+        mVolumeUtils.onDestroy();
     }
 
     @Override
