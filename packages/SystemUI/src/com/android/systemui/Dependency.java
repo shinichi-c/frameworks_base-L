3ea6c2f53994a37c4f278bb5a9f43a4d19687c0a
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-06 15:28:34 +0800

    [AxionUI] Rework back gesture arrow
    Change-Id: I145e9ba76bfa6bc36b2528012c2ee9a1fc83db9c
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -58,6 +58,7 @@ import com.android.systemui.tuner.TunerService;
 import dagger.Lazy;
 
 import java.util.function.Consumer;
+import java.util.concurrent.Executor;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -113,6 +114,17 @@ public class Dependency {
     private final ArrayMap<Object, Object> mDependencies = new ArrayMap<>();
     private final ArrayMap<Object, LazyDependencyCreator> mProviders = new ArrayMap<>();
 
+    /**
+     * Generic executor on a background thread.
+     */
+    private static final String BACKGROUND_EXECUTOR_NAME = "background_executor";
+
+    /**
+     * Generic executor on a background thread.
+     */
+    public static final DependencyKey<Executor> BACKGROUND_EXECUTOR =
+            new DependencyKey<>(BACKGROUND_EXECUTOR_NAME);
+
     @Inject DumpManager mDumpManager;
 
     @Inject Lazy<BroadcastDispatcher> mBroadcastDispatcher;
@@ -149,6 +161,7 @@ public class Dependency {
     @Inject Lazy<DialogTransitionAnimator> mDialogTransitionAnimatorLazy;
     @Inject Lazy<UserTracker> mUserTrackerLazy;
     @Inject Lazy<StatusBarWindowControllerStore> mStatusBarWindowControllerStoreLazy;
+    @Inject @Background Lazy<Executor> mBackgroundExecutor;
 
     @Inject
     public Dependency() {
@@ -194,6 +207,7 @@ public class Dependency {
         mProviders.put(UserTracker.class, mUserTrackerLazy::get);
         mProviders.put(
                 StatusBarWindowControllerStore.class, mStatusBarWindowControllerStoreLazy::get);
+        mProviders.put(BACKGROUND_EXECUTOR, mBackgroundExecutor::get);
 
         Dependency.setInstance(this);
     }
