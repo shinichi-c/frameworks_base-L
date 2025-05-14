972a1092d8a5049a3fb8d19fde42f96d91ff8dee
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-14 16:40:04 +0800

    [FEATURE]: add nothingOS always-on-display changes
    reverse-engineered from nothingOS 3.0
    
    Change-Id: Iab6bd5f7a6169aacec914a7e423e27f2ebaccd70
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
@@ -2475,4 +2475,8 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
         void onFlingQsWithoutClick(ValueAnimator animator, float qsExpansionHeight,
                 float target, float vel);
     }
+
+    public boolean isVisible() {
+        return mVisible;
+    }
 }
