604d90edf751cd91fa8cca7560869f3a4bcb1392
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-30 06:22:04 +0800

    services: implement app launch optimization tweak
    from code linaro:
    Freezer is added to enhance app launch and optimize app launch/exit
    animation. It mainly offload BG CPU resource by freezing other
    processes during app launch.
    
    Change-Id: I7eb44e0f5b3ec443b7858f790f0f1bad9615ad71
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -337,6 +337,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.Parcel;
 import android.os.ParcelFileDescriptor;
+import android.os.PerformanceHintManager;
 import android.os.PowerExemptionManager;
 import android.os.PowerExemptionManager.ReasonCode;
 import android.os.PowerExemptionManager.TempAllowListType;
@@ -973,6 +974,10 @@ public class ActivityManagerService extends IActivityManager.Stub
         final int pid = app.getPid();
         synchronized (mPidsSelfLocked) {
             mPidsSelfLocked.doAddInternal(pid, app);
+            ProcessFreezerManager freezer = ProcessFreezerManager.getInstance();
+            if (freezer != null && freezer.useFreezerManager()) {
+                freezer.addPidLocked(app);
+            }
         }
         synchronized (sActiveProcessInfoSelfLocked) {
             if (app.processInfo != null) {
@@ -994,6 +999,11 @@ public class ActivityManagerService extends IActivityManager.Stub
         final boolean removed;
         synchronized (mPidsSelfLocked) {
             removed = mPidsSelfLocked.doRemoveInternal(pid, app);
+            ProcessFreezerManager freezer = ProcessFreezerManager.getInstance();
+            if (freezer != null && freezer.useFreezerManager()) {
+                freezer.removePidLocked(pid, app);
+                freezer.startUnfreeze(app.processName, ProcessFreezerManager.REMOVE_PROCESS_UNFREEZE);
+            }
         }
         if (removed) {
             synchronized (sActiveProcessInfoSelfLocked) {
@@ -5269,6 +5279,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         showMteOverrideNotificationIfActive();
 
         t.traceEnd();
+        ProcessFreezerManager.getInstance().init(mFreezer);
     }
 
     private void showConsoleNotificationIfActive() {
