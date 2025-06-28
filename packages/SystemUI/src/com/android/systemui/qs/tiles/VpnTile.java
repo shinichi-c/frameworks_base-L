ef2629948c9617678bbb5a3fe9fb4d0e84f1bff7
Pranav Vashi <neobuddy89@gmail.com>
2025-06-29 01:48:05 +0530

    SystemUI: tiles: Preload all icon drawable for custom tiles
    Ref: ce8bee1b93af62a57097ca60c36265488a1d9371
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/VpnTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/VpnTile.java
@@ -66,6 +66,9 @@ public class VpnTile extends SecureQSTile<BooleanState> {
 
     public static final String TILE_SPEC = "vpn";
 
+    @Nullable
+    private Icon mIcon = null;
+
     private final SecurityController mController;
     private final KeyguardStateController mKeyguard;
     private final PanelInteractor mPanelInteractor;
@@ -195,7 +198,11 @@ public class VpnTile extends SecureQSTile<BooleanState> {
         state.value = mController.isVpnEnabled();
         state.secondaryLabel = mController.getPrimaryVpnName();
         state.contentDescription = state.label;
-        state.icon = ResourceIcon.get(R.drawable.ic_qs_vpn);
+        if (mIcon == null) {
+            mIcon = maybeLoadResourceIcon(
+                    R.drawable.ic_qs_vpn);
+        }
+        state.icon = mIcon;
         boolean hasAnyVpn = mController.getConfiguredLegacyVpns().size() > 0
                 || mController.getVpnAppPackageNames().size() > 0;
         if (mController.isVpnRestricted() || !hasAnyVpn) {
