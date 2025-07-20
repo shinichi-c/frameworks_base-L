6b945b3dbaeab289946479b44ef61bc610ab26b5
Ghosuto <clash.raja10@gmail.com>
2025-07-20 09:19:21 +0000

    SystemUI: Observe some settings
    Change-Id: Ib68ffdfcaa75690fe66b69cb0700147f37ef8e7e

--- packages/SystemUI/src/com/android/systemui/theme/RisingSettingsConstants.java
+++ packages/SystemUI/src/com/android/systemui/theme/RisingSettingsConstants.java
@@ -20,9 +20,18 @@ import android.provider.Settings;
 public class RisingSettingsConstants {
 
     public static final String[] SYSTEM_SETTINGS_KEYS = {
+        "status_bar_logo",
+        "status_bar_logo_color",
+        "statusbar_colored_icons",
+        "statusbar_notif_count",
+        "ls_media_art_enabled",
+        "ls_media_art_filter",
+        "ongoing_action_chip",
+        "compact_progress_mode"
     };
     
     public static final String[] SECURE_SETTINGS_KEYS = {
+        "volume_show_media_button"
     };
     
     public static final String[] SYSTEM_SETTINGS_NOTIFY_ONLY_KEYS = {
